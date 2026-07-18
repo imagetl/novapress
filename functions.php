64a4edbe2db487b326ecace5cf2bdb3960ee3ebf
Anwarul Islam <imagetl@gmail.com>
2026-07-18 22:00:33 +0600

    Add theme setup and Wordpress support

--- functions.php
+++ functions.php
@@ -7,3 +7,60 @@
 </head>
 
 <body <?php body_class(); ?>>
+    
+<?php
+/**
+ * Theme functions
+ *
+ * @package NovaPress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Theme Setup
+ */
+function novapress_setup() {
+
+	// Let WordPress manage the document title.
+	add_theme_support( 'title-tag' );
+
+	// Featured Image Support.
+	add_theme_support( 'post-thumbnails' );
+
+	// Custom Logo Support.
+	add_theme_support(
+		'custom-logo',
+		array(
+			'height'      => 100,
+			'width'       => 300,
+			'flex-height' => true,
+			'flex-width'  => true,
+		)
+	);
+
+	// HTML5 Support.
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		)
+	);
+
+	// Register Navigation Menu.
+	register_nav_menus(
+		array(
+			'primary' => 'Primary Menu',
+		)
+	);
+}
+
+add_action( 'after_setup_theme', 'novapress_setup' );
