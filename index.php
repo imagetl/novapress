c9a03314a2d16e3ffe45cb172728070ba40fbb7b
Anwarul Islam <imagetl@gmail.com>
2026-07-18 19:18:05 +0600

    initial stage

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Main template file
+ *
+ * @package NovaPress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+?>
+
+<main class="site-main">
+	<h1>Welcome to NovaPress</h1>
+	<p>My first custom WordPress theme.</p>
+</main>
+
+<?php
+get_footer();
