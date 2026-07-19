ce1072d66ab649e177ef1d396163aa2f621adbca
Anwarul Islam <imagetl@gmail.com>
2026-07-19 07:26:44 +0600

    Fix invalid JSON response by removing HTML from function.php

--- single.php
+++ single.php
@@ -4,20 +4,49 @@ get_header();
 
 <main class="site-main">
 
-<h1>Single Post</h1>
+<?php
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+?>
+
+<article class="post">
+
+	<?php
+	if ( has_post_thumbnail() ) {
+		the_post_thumbnail( 'large' );
+	}
+	?>
+
+	<h1><?php the_title(); ?></h1>
+
+	<p>
+		Published:
+		<?php echo esc_html( get_the_date( 'F j, Y' ) ); ?>
+		|
+		Author:
+		<?php the_author(); ?>
+	</p>
+
+	<p>
+		Category:
+		<?php the_category( ', ' ); ?>
+	</p>
+
+	<?php the_content(); ?>
+
+	<p>
+		Tags:
+		<?php the_tags( '', ', ', '' ); ?>
+	</p>
+
+	<?php comments_template(); ?>
+
+</article>
 
 <?php
-
-while(have_posts()):
-
-the_post();
-
-the_title('<h2>','</h2>');
-
-the_content();
-
-endwhile;
-
+	endwhile;
+endif;
 ?>
 
 </main>
